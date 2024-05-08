e2162d54b81b28f7e965c8b8c4f52014aa7308aa
HYUNWOO CHEON <milcamboo@gmail.com>
2024-05-08 19:07:13 +0900

    규칙성을 찾아서 풀이

--- level2/점프와순간이동.py
+++ level2/점프와순간이동.py
@@ -1,11 +1,13 @@
 class JumpAndTeleport:
+
+    @staticmethod
+    def solution_with_bin(self, n: int) -> int:
+        return bin(n).count('1')
+
     @staticmethod
     def solution(n: int) -> int:
-        memoization = [0, 1, 1, 2]
-        for i in range(4, n + 1):
-            if i % 2 == 0:
-                memoization.append(min(memoization[i // 2], memoization[i - 1] + 1))
-            else:
-                memoization.append(memoization[i - 1] + 1)
-
-        return memoization[n]
+        ans = 1
+        while n > 1:
+            ans += n % 2
+            n = n // 2
+        return ans
